8b84e5cba606194a84fbb63e32b7c927fc389342
milli <milli@u.northwestern.edu>
2020-02-04 15:40:56 -0600

    threading; one client w/ droneID+mode; untested

--- user_input/src/client.cpp
+++ user_input/src/client.cpp
@@ -49,16 +49,20 @@ int i, i_temp, i_char;
 // Set values to 0 to prepare for the start of input
 int num_values_in_message=0;
 int comma_index = 0;
+// Set done and num_index as if "Done" message was just sent
 int done = 0;
+int num_index = 2
 
 using namespace std;
 // Define global variables for postition of person_location
 int xloc = 15;
 int yloc = 15;
 float th = 0;
+int x_middle = 15;
+int y_middle = 15;
 string location_string = "?";
 char location_chararray[MAXLEN];
-
+char input_mode;
 int gridsize_unity = 30;
 
 void chatterCallback(const user_input::person_position::ConstPtr& msg)
@@ -73,8 +77,10 @@ void chatterCallback(const user_input::person_position::ConstPtr& msg)
 
 int main(int argc , char *argv[])
 {
-// Set up ros node
-  ros::init(argc, argv, "client_sharedcontrol");
+  /////////////////////////////////////////////////////////////////
+  //  Set up ros node
+  /////////////////////////////////////////////////////////////////
+  ros::init(argc, argv, "client");
   ros::NodeHandle n;
   ros::Rate rate(10.0);
 
@@ -82,6 +88,7 @@ int main(int argc , char *argv[])
   ros::Publisher pub = n.advertise<user_input::input_array>("input", 1000);
   user_input::input_array send_array;
   // Send empty array so that the topic can be found even if no inputs have been made
+  send_array.droneID = 10;
   send_array.xinput.clear();
   send_array.yinput.clear();
   send_array.datalen = 0;
@@ -91,17 +98,9 @@ int main(int argc , char *argv[])
   // Set up Subscriber
   ros::Subscriber sub = n.subscribe("person_location", 1000, chatterCallback);
 
-    // Uncomment to publish a "person_location" message
-    // ros::Publisher pub2 = n.advertise<user_input::person_position>("person_location", 1000);
-    // user_input::person_position trythis;
-    // trythis.xpos = 4;
-    // trythis.ypos = 4;
-    // trythis.theta = 4;
-    // pub2.publish(trythis);
-    // printf("Published \n");
-    // ros::spinOnce();
-
-// Setting up TCP socket
+  /////////////////////////////////////////////////////////////////
+  // Setting up TCP socket
+  /////////////////////////////////////////////////////////////////
 	int socket_desc;
 	struct sockaddr_in server;
 	char *message, server_reply[MAXLEN];
@@ -125,7 +124,9 @@ int main(int argc , char *argv[])
 	}
 	puts("Connected to Windows Server");
 
-// Testing connection
+  /////////////////////////////////////////////////////////////////
+  // Testing connection
+  /////////////////////////////////////////////////////////////////
 	//Send data for test
 	message = "Testing connection";
 	if( send(socket_desc , message , strlen(message) , 0) < 0)
@@ -142,76 +143,67 @@ int main(int argc , char *argv[])
 	puts("Reply received");
 	puts("Test completed");
 
-  // int count = 0;
+  /////////////////////////////////////////////////////////////////
+  // Continuously publish recieved messages
+  /////////////////////////////////////////////////////////////////
   while (ros::ok())
   {
-    // Keep track of iterations
-	  // count=count+1;
-
-    //puts("New loop");
     // Recieve message from server
 		if( recv(socket_desc, message_from_server , MAXLEN , 0) < 0)
 		{
 			puts("recv failed");
 		}
-
     // Print message
 		//	puts(message_from_server);
 		//printf("\n");
 
-    // If the server is done sending the coordinates, publish message
+    // Publish message when the server is done sending the coordinates
     if ((message_from_server[0]=='D') &&
         (message_from_server[1]=='o') &&
         (message_from_server[2]=='n') &&
         (message_from_server[3]=='e')) {
             // Ensure that only one message is published when done
             if (done==0) {
-              printf("Size of array %d \n",num_values_in_message);
-
-              //printf("Size of x-array %d \n",len(send_array.xinput));
-              //printf("Size of x-array %d \n",len(send_array.yinput));
 
               // Set array length and publish to /input
               send_array.datalen = num_values_in_message;
               pub.publish(send_array);
-              printf("Input coordinates published \n");
+              printf("Input coordinates published w/ size %d \n",num_values_in_message);
 
               // Clear send_array values and set values to zero to prepare for new message
-    		      // send_array.datalen.clear();
     		      send_array.xinput.clear();
               send_array.yinput.clear();
-    		      //send_array.xinput={};
-              //send_array.yinput={};
+              send_array.droneID = 10;
               comma_index = 0;
               num_values_in_message=0;
               send_array.datalen = num_values_in_message;
 
               ros::spinOnce(); // Standard line to allow services
-              sleep(1);
+              sleep(0.5);
 
               // Publish the empty array
               pub.publish(send_array);
               printf("Empty array published \n");
 
               ros::spinOnce(); // Standard line to allow services
-              sleep(1);
+              sleep(0.5);
             }
             done = 1;
-    } else {
-      // trythis.xpos = count;
-      // trythis.ypos = 4;
-      // trythis.theta = 4;
-      // pub2.publish(trythis);
-      // printf("Published \n");
-      // ros::spinOnce();
+    } else
+    // Store coordinates in message from server
+    {
       // Strings follow format "x-coor,y-coor,....x-coor,y-coor,!"
-      //printf("New string containing coordinates \n");
+      // Define variables for unpacking string
       flag_exclaim = 0; // flag for exclaimation mark
       flag_comma = 0; // flag for comma
-      done = 0; // reset done flag because new set of coordinates are being sent
       coordinate = 'x'; // string starts with x-coordinate
       comma_index = 0; // The string starts with the first number so the imaginary comma is at location -1
 
+      // If it the beginning of a new message
+      if (done==1) {
+          done = 0; // reset done flag because new set of coordinates are being sent
+          num_index = 0; // set the num_index that keeps track of the index of the number in the list, important for knowing which is for droneID and mode
+      }
       // Iterate through the characters in latest message from server
       for (i=0;i<MAXLEN;i++)
   		{
@@ -237,40 +229,57 @@ int main(int argc , char *argv[])
           temp_char[i_temp]='.'; // atoi recognized a '.' as indicating the end of a number
           temp_number = atoi(temp_char); // from std package
           //printf( "temp_number: %d \n", temp_number);
-          
-          // Append send_array message with new value
-          // Values alternate between x and y coordinates
-          if (coordinate == 'x') {
+
+          // Determine if the number is a droneID, mode, x-coordinate or y-coordinate
+          // After the first two values, values alternate between x and y coordinates
+          if (num_index == 0) // the first in the set of messages is a droneID
+          {
+            send_array.droneID = temp_number;
+            num_index++;
+          } else if (num_index == 1) // the second number is the view (arial = )
+          {
+            input_mode = temp_char[0];
+            num_index++;
+          } else if (coordinate == 'x') {
             temp_x = temp_number;
             coordinate = 'y';
             //printf("coordinate x found %f \n", temp_x);
           } else if (coordinate == 'y') {
-
-            // The points recieved here are in relative the the person's position
-            // and direction in a unity's scale
-
-            // Rotate backwards relative to person
             temp_y = temp_number;
-            // xloc = 15;
-            // yloc = 15;
-            // th = 0.0f;
+            coordinate = 'x';
             //printf("coordinate y found %f \n", temp_y);
-            float temp_x2 = temp_x*cos(-th)-temp_y*sin(-th);
-            float temp_y2 = temp_x*sin(-th)+temp_y*cos(-th);
-            // Find absolute coordinates not relative
-            int temp_x3 = round(temp_x2 + xloc);
-            temp_y2 = temp_y2 + (gridsize_unity - yloc); // finish conversion, uncomment when using UWP touch program
-            int temp_y3 = round(gridsize_unity - temp_y2); // finish conversion, uncomment when using UWP touch program
-            printf("Transformed coordinates- x: %d y: %d \n",temp_x3,temp_y3);
 
+            // Perform the rest of the coordinate transform depending on input_mode
+            if (input_mode=='l') {
+              // The points recieved here are in relative the the person's position
+              // and direction in a unity's scale
+
+              // Rotate backwards relative to person
+              float temp_x2 = temp_x*cos(-th)-temp_y*sin(-th);
+              float temp_y2 = temp_x*sin(-th)+temp_y*cos(-th);
+
+              // Find absolute coordinates not relative
+              int temp_x3 = round(temp_x2 + xloc);
+              temp_y2 = temp_y2 + (gridsize_unity - yloc); // finish conversion, uncomment when using UWP touch program
+              int temp_y3 = round(gridsize_unity - temp_y2); // finish conversion, uncomment when using UWP touch program
+
+              printf("Transformed coordinates- x: %d y: %d \n",temp_x3,temp_y3);
+            } else if (input_mode=='g') {
+              // Find absolute coordinates not relative
+              int temp_x3 = round(temp_x + x_middle);
+              float temp_y2 = temp_y + (gridsize_unity - y_middle); // finish conversion, uncomment when using UWP touch program
+              int temp_y3 = round(gridsize_unity - temp_y2); // finish conversion, uncomment when using UWP touch program
+              printf("Transformed coordinates- x: %d y: %d \n",temp_x3,temp_y3);
+
+            }
+
+            // If within grid, append array message with values
             if ((temp_x3>=0) && (temp_x3<=29) && (temp_y3>=0) && (temp_y3<=29)){
                 send_array.xinput.push_back(temp_number); // add to message
                 send_array.yinput.push_back(temp_number);
                 num_values_in_message++;
-		        printf("adds values to message- x: %d y: %d \n ",temp_x3,temp_y3);
-		 
+		            // printf("adds values to message- x: %d y: %d \n ",temp_x3,temp_y3);
             }
-            coordinate = 'x';
           }
           comma_index=i; // Store latest comma location for use later
           flag_comma = 0; // reset flag
@@ -278,17 +287,10 @@ int main(int argc , char *argv[])
       }
 		}
 
-    // Reply to server (Later I imaging sending the location of the person here
-        // and editing the code to continue looping at a set rate)
+    // Reply to server with the person's location
     strcpy(location_chararray,location_string.c_str());
     int stringlen = location_string.length();
-    // if ((stringlen==0)  || (stringlen==1))
-    // {
-    //   stringlen=5;
-    //   printf("string length is zero");
-    // }
-		//puts("Sending reply");
-    //printf("location_chararray len %d \n",strlen(location_chararray));
+    // Send pre-set error message if the the person_position has not been published
     if ((strlen(location_chararray)==0) || (strlen(location_chararray)==1))
     {
        location_chararray[0]='?';
@@ -297,7 +299,6 @@ int main(int argc , char *argv[])
        stringlen=5;
        //printf("location set");
     }
-		//message = "Recieved";
 		if( send(socket_desc , location_chararray , stringlen , 0) < 0)
 		{
 			puts("Send failed");
@@ -307,7 +308,6 @@ int main(int argc , char *argv[])
     ros::spinOnce();
     rate.sleep();
     //sleep(1);
-
 	} // Continue looping until ctrl c is pressed
 	return 0;
 }
